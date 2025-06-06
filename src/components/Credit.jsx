27b6acf697fae26ee914b74abde49a8c3127d008
Bhaskar Saini <bhaskarsaini333@gmail.com>
2025-06-06 18:55:57 +0530

    change credit fade sec to 1000

--- src/components/Credit.jsx
+++ src/components/Credit.jsx
@@ -6,7 +6,7 @@ import '../styles/Credit.css'
 const Credit = () => {
     return (
         <div className="footer">
-            <div className="credit" data-aos = "fade" data-aos-delay={2000}>
+            <div className="credit" data-aos = "fade" data-aos-delay={1000}>
                 <div>Built and designed by Bhaskar Saini. Inspired from Gazi Jarin.</div>
                 <div>All rights reserved. ©</div>
             </div>
